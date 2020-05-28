296096e886872299adeacb59ac04a5ff6f92dc56
Henry Herrera <henryjherreram@gmail.com>
2020-05-28 12:50:52 -0700

    2nd files commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next)=>{
     // console.log('cookie val: ', req.get('Cookie'));
     
@@ -15,10 +17,23 @@ exports.getLogin = (req, res, next)=>{
 exports.postLogin = (req, res, next)=>{
     // req.isLoggedIn = true;
     // res.setHeader('Set-Cookie', 'loggedIn=true');
-    req.session.isLoggedIn = true;
-    res.redirect('/')
+    User.findById('5ecfe2efa541193ce0925e77')
+    .then(user => {
+        req.session.isLoggedIn = true;
+        req.session.user = user;
+        req.session.save((err)=>{
+            console.log(err);
+            res.redirect('/');
+        });
+    })
+    .catch(err => console.log(err));
+
+    // req.session.isLoggedIn = true;
+    // res.redirect('/')
     // console.log("session: ", req.session.isLoggedIn);
-}
+};
+    
+
 
 exports.postLogout = (req,res,next) => {
 
